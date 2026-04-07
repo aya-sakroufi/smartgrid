703e464d8e5d2f2dd1fdb834f64c8aca89d844ea
Ayari Wiem <wiem.ayari@etudiant-isi.utm.tn>
2026-04-07 20:10:56 +0100

    Delete stop.sh

--- stop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Arrêt des serveurs..."
-pkill -9 -f "python app.py" 2>/dev/null || echo "Backend déjà arrêté"
-pkill -9 -f "npm start" 2>/dev/null || echo "Frontend déjà arrêté"
-echo "Fait."
